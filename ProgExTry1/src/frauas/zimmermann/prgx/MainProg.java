d589e5399b19ee716d18bf500647596304b38eb5
Ivan2344 <Ivan2344>
2024-04-26 14:33:36 +0200

    Hoofentlich workes

---
+++ ProgExTry1/src/frauas/zimmermann/prgx/MainProg.java
@@ -0,0 +1,10 @@
+package frauas.zimmermann.prgx;
+
+public class MainProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
