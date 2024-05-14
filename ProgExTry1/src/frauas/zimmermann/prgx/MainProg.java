a055f80e06f1cebf4b4dc142c177d6b3476e4fc3
Sofia <papadopoulou.s455@gmail.com>
2024-05-14 12:06:20 +0200

    anders

--- ProgExTry1/src/frauas/zimmermann/prgx/MainProg.java
+++ ProgExTry1/src/frauas/zimmermann/prgx/MainProg.java
@@ -7,7 +7,7 @@ public class MainProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.print("Lolz");
+		System.out.print("Lolz3");
 	}
 
 }
