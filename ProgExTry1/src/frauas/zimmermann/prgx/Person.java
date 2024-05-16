509b02b50c5702683e7f86f3dbf1b188c819e01b
Sofia <papadopoulou.s455@gmail.com>
2024-05-16 13:44:12 +0200

    new class commited2

---
+++ ProgExTry1/src/frauas/zimmermann/prgx/Person.java
@@ -0,0 +1,55 @@
+package frauas.zimmermann.prgx;
+
+public class Person {
+	public int id;
+	public int gender;
+    public String nnam;
+    public String fnam;
+    public String str;
+    public String tel;
+    public String mobil;
+    
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getNnam() {
+		return nnam;
+	}
+	public void setNnam(String nnam) {
+		this.nnam = nnam;
+	}
+	public String getFnam() {
+		return fnam;
+	}
+	public void setFnam(String fnam) {
+		this.fnam = fnam;
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getMobil() {
+		return mobil;
+	}
+	public void setMobil(String mobil) {
+		this.mobil = mobil;
+	}
+	
+}
