8b2db04c116818751d256d02cbccdf65ceebaa6f
Jpaaazz <jschoenfeld196@gmail.com>
2024-06-04 14:26:45 +0200

    panel display should work now

--- ProgExTry1/src/frauas/zimmermann/prgx/GUI.java
+++ ProgExTry1/src/frauas/zimmermann/prgx/GUI.java
@@ -1,139 +1,192 @@
 package frauas.zimmermann.prgx;
 
+
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListCellRenderer;
-import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class GUI extends Mainframe {
-	JFrame innerFrame;
-	DefaultListModel <String> model = new DefaultListModel();
-	JPanel[] panelList;
-	String[] menuItems;
-	
-	GUI(){
-		super();
-		initializeGui();
-	}
-	public void initializeGui(){
-		 String[] menuItems = {"Orders", "Products", "Employees", "Customers", "New Order"};
-	        createMenuList(menuItems);
+    JPanel[] panelList;
+    String[] menuItems = {"Orders", "Products", "Employees", "Customers"};
+
+    GUI() {
+        super();
+        createMainPanel();
+        createMenuList(menuItems);
+        northCenterPanel.revalidate();
+        northCenterPanel.repaint();
+        
+    }
+
+    public void createMenuList(String[] items) {
+        JList<String> menuList = new JList<>(items);
+        menuList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        menuList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        menuList.setVisibleRowCount(1); // Show all items
+        menuList.setFont(new Font("Arial", Font.PLAIN, 18));
+        menuList.setBackground(Color.DARK_GRAY);
+        menuList.setForeground(LIGHT_BLUE);
+
+        int cellWidth = northCenterPanel.getPreferredSize().width / items.length;
+        menuList.setFixedCellWidth(cellWidth);
+        menuList.setFixedCellHeight(50);
+
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) menuList.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+        menuList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    int selectedIndex = menuList.getSelectedIndex();
+                    for (int i = 0; i < panelList.length; i++) {
+                        if (i == selectedIndex) {
+                            panelList[i].setVisible(true);
+                            System.out.print("item click is recognized\n");
+                            System.out.print(i);
+                            System.out.print("\n");
+                        } else {
+                            panelList[i].setVisible(false);
+                            System.out.print("item click is recognized empty\n");
+                        }
+                    }
+                    innerCenterPanel.revalidate();
+                    innerCenterPanel.repaint();
+                }
+            }
+        });
+
+
+        northCenterPanel.add(menuList);
+    }
+
+
+    public void createMainPanel() {
+        panelList = new JPanel[menuItems.length];
+        innerCenterPanel.setLayout(new CardLayout());
+        
+        for (int i = 0; i < menuItems.length; i++) {
+            panelList[i] = setMainPanels(i);
+//            panelList[i] = setContents(i); 
+            panelList[i].setVisible(true);
+            innerCenterPanel.add(panelList[i]);
+        }
+        
+        frame.add(center, BorderLayout.CENTER);
+    }
+
+    public JPanel setMainPanels(int menuItem) {
+        JPanel tempPanel = new JPanel();
+        tempPanel.setBackground(Color.WHITE);
+        tempPanel.setLayout(new GridLayout(1, 2));
+
+        
+        JPanel leftPanel = createPanelWithBorder(LEFT_PANEL);
+        JPanel rightPanel = createPanelWithBorder(RIGHT_PANEL);        
+        JPanel firstLeftPanel = createPanelWithBorder(INNER_LEFT_PANEL);
+		JPanel secondLeftPanel = createPanelWithBorder(INNER_LEFT_PANEL);
 		
-		northCenterPanel.revalidate();
-		northCenterPanel.repaint();
 		
-	}
-//	public void createMenuButton(String buttonName, Color color) {
-////		JPanel buttonPanel = new JPanel();
-//		JButton menuButton = new JButton(buttonName);
-//		
-//		menuButton.setBackground(color);
-//		northCenterPanel.add(menuButton);
-//	}
-	 public void createMenuList(String[] items) {
-		 JList<String> menuList = new JList<>(items);
-	        menuList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	        menuList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-	        menuList.setVisibleRowCount(1); // Show all items
-	        menuList.setFont(new Font("ARIAL", Font.PLAIN, 18));
-	        menuList.setBackground(Color.DARK_GRAY);
-	        menuList.setForeground(LIGHT_BLUE);
+        JLabel firstLabel = new JLabel(String.valueOf(menuItem), SwingConstants.CENTER);
+        firstLeftPanel.add(firstLabel);
+        JLabel secondLabel = new JLabel("Second Left Panel", SwingConstants.CENTER);
+        secondLeftPanel.add(secondLabel);
+        JLabel rightLabel = new JLabel("Right Panel", SwingConstants.CENTER);        
+        rightPanel.add(rightLabel);
+        
+        
+        leftPanel.add(firstLeftPanel);
+        leftPanel.add(secondLeftPanel);
+        tempPanel.add(leftPanel);
+        tempPanel.add(rightPanel);
+        
+//        setContents(menuItem);
+        
+        return tempPanel;
+    }
 
-	        // Calculate the width for each cell dynamically
-	        int cellWidth = northCenterPanel.getPreferredSize().width / items.length;
-	        menuList.setFixedCellWidth(cellWidth);
-	        menuList.setFixedCellHeight(50);
+    private JPanel createPanelWithBorder(int panelType) {
+        JPanel panel = new JPanel();
+        Color myColor = new Color(230, 230, 255);
+        panel.setBackground(myColor);
 
-	        // Center the text horizontally
-	        DefaultListCellRenderer renderer = (DefaultListCellRenderer) menuList.getCellRenderer();
-	        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+        int topBorderWidth, leftBorderWidth, bottomBorderWidth, rightBorderWidth;
 
-	        northCenterPanel.add(menuList);
+        switch (panelType) {
+            case RIGHT_PANEL:
+                topBorderWidth = 100;
+                leftBorderWidth = 20;
+                bottomBorderWidth = 100;
+                rightBorderWidth = 20;
+                break;
+            case INNER_LEFT_PANEL:
+                topBorderWidth = 0;
+                leftBorderWidth = 10;
+                bottomBorderWidth = 100;
+                rightBorderWidth = 10;
+                break;
+            default: // LEFT_PANEL
+                topBorderWidth = 100;
+                leftBorderWidth = 10;
+                bottomBorderWidth = 0;
+                rightBorderWidth = 10;
+                break;
+        }
 
-		    northCenterPanel.add(menuList);
-	    }
-	 public void createMainPanel() {
-		 
-	  JPanel container = new JPanel();
-	  container.setLayout(null);
-	  for (int i = 0; i < menuItems.length;i++) {
-			panelList[i]= setMainPanel(i);
-			panelList[i].setVisible(false);
-			panelList[i].setBounds(0, 0, 1275, 1000);
-			center.add(panelList[i]);
-		}
-		frame.add(center, BorderLayout.CENTER);
-	}
-	 
-	 public JPanel setMainPanel(int i) {
-		 JPanel tempPanel = new JPanel();
-		 
-		 return tempPanel;
-	 }
-//    mainPanel = new JPanel();
-//    mainPanel.setBackground(Color.WHITE);
-//    mainPanel.setLayout(new GridLayout(1, 2));
-//    
-//    JPanel leftPanel = createPanelWithBorder(LEFT_PANEL);
-//    JPanel rightPanel = createPanelWithBorder(RIGHT_PANEL);
-//    
-//    JPanel firstLeftPanel = createPanelWithBorder(INNER_LEFT_PANEL);
-//    JPanel secondLeftPanel = createPanelWithBorder(INNER_LEFT_PANEL);
-//    
-//    
-//    leftPanel.add(firstLeftPanel);
-//    leftPanel.add(secondLeftPanel);
-//    
-//    mainPanel.add(leftPanel);
-//    mainPanel.add(rightPanel);
-	 
-	private JPanel createPanelWithBorder(int panelType) {
-		JPanel panel = new JPanel();
-		Color myColor = new Color(230,230,255);
-	    panel.setBackground(myColor);
-	    
-	    int topBorderWidth, leftBorderWidth, bottomBorderWidth, rightBorderWidth;
-	    
-	    
-	    switch(panelType){
-	    	case RIGHT_PANEL: 
-	    		topBorderWidth = 100;
-	    		leftBorderWidth = 20;
-	    		bottomBorderWidth = 100;
-	    		rightBorderWidth = 20;
-	    		break;
-	    	case INNER_LEFT_PANEL:
-				 topBorderWidth = 0; 
-				 leftBorderWidth = 10;
-			     bottomBorderWidth = 100;
-			     rightBorderWidth = 10;
-				 break;
-	    	case LEFT_PANEL:
-					topBorderWidth = 100; 
-				 leftBorderWidth = 10;
-			     bottomBorderWidth = 0;
-			     rightBorderWidth = 10;
-				 break;
-				default:
-				 topBorderWidth = 0; 
-	                leftBorderWidth = 0;
-	                bottomBorderWidth = 0;
-	                rightBorderWidth = 0;
-	                break;
-			}
-		
-		panel.setBorder(BorderFactory.createMatteBorder(
-		        topBorderWidth, leftBorderWidth, bottomBorderWidth, rightBorderWidth, BORDER_COLOR));
-		    panel.setLayout(new GridLayout(1, 2));
-	    return panel;
-	}
+        panel.setBorder(BorderFactory.createMatteBorder(
+                topBorderWidth, leftBorderWidth, bottomBorderWidth, rightBorderWidth, BORDER_COLOR));
+        panel.setLayout(new GridLayout(1, 2));
+        return panel;
+    }
+    
+//    public JPanel setContents(int i) {
+//    	JPanel contentPanel = new JPanel();
+//    	
+//    	switch(i){
+//    	case 0: 
+//    		
+////    		JButton addButton = new JButton("Add");
+////    		JButton deleteButton = new JButton("Delete");
+////    		JButton editButton = new JButton("Edit");
+////    		JButton refreshButton = new JButton("Refresh");
+//    		
+//    		
+//    		break;
+//    	case 1: 
+//    		 
+//    	    break;
+//    	    
+//    	case 2: 
+//    		
+//    	case 3: 
+//   		
+// 	    break;
+//    	        
+//    	        
+//    	       
+//    		
+//    		
+//    	}
+//    	
+//    	  	        
+//    	return contentPanel;
+//    }
+    
+    
+    
 }
