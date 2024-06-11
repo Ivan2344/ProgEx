962d24e10cf38ee49c7919cf07360341280f38c5
Ivan2344 <Ivan2344>
2024-06-11 11:56:49 +0200

    changed main

--- ProgExTry1/src/frauas/zimmermann/prgx/MainProg.java
+++ ProgExTry1/src/frauas/zimmermann/prgx/MainProg.java
@@ -5,10 +5,12 @@ import java.sql.*;
 public class MainProg {
 
 	public static void main(String[] args) {
+		String username = "";
+		String pwd = "";
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.print("Lolz3");
-		Create_Shema c1 = new Create_Shema("", "");
+		Create_Shema c1 = new Create_Shema("username", "pwd");
 		c1.CreateDB();
 		c1.setSch("jdbc:mysql://localhost:3306/SEIFENdemo2");
 		c1.InsertDemoValues();
